08e3dffbaa6f4bab251b1d5e8d1a0f48f636f813
xiangyj <877725296@qq.com>
2020-07-01 22:41:57 +0800

    first commit

---
+++ src/balancer/WeightedRoundRobinBalancer.java
@@ -0,0 +1,88 @@
+package balancer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 加权轮训负载均衡算法
+ * @param <T>
+ */
+public class WeightedRoundRobinBalancer<T> {
+
+    private static final WeightedRoundRobinBalancer INSTANCE = new WeightedRoundRobinBalancer<>();
+    private WeightedRoundRobinBalancer() {}
+
+    public static WeightedRoundRobinBalancer<String> instance() {
+        return INSTANCE;
+    }
+    /** 当前平衡器持有的负载因子*/
+    private static final List<Node> invokers = new ArrayList<>();
+
+    /** 所有负载因子权重之和*/
+    private volatile static int totalWeight;
+
+    /**
+     * 添加负载因子
+     * @param t 类型
+     * @param weight 权重
+     */
+    public void addInvoker(T t, int weight) {
+        invokers.add(new Node<>(t, weight));
+        totalWeight += weight;
+    }
+
+    /**
+     * 返回下一个负载因子
+     * 该算法的原理如下：
+     *
+     * 　　每个服务器都有两个权重变量：
+     * 　　a：weight，配置文件中指定的该服务器的权重，这个值是固定不变的；
+     * 　　b：current_weight，服务器目前的权重。一开始为0，之后会动态调整。
+     *
+     * 　　每次当请求到来，选取负载因子时，会遍历数组中所有负载因子。对于每个负载，让它的current_weight增加它的weight；同时累加所有服务器的weight，并保存为total。（一般可认为保存不变）
+     * 　　遍历完所有因子之后，如果该因子的current_weight是最大的，就选择这个处理本次请求。最后把该因子的current_weight减去total。
+     * @return T
+     */
+    public T nextInvoker() {
+        Node<T> result = null;
+        int weight = -totalWeight;
+        for (Node<T> node : invokers) {
+            int currentWeight = node.currentWeight + node.weight;
+            // 判断最大
+            if (currentWeight >= weight) {
+                result = node;
+                weight = currentWeight;
+            }
+            node.currentWeight = currentWeight;
+        }
+        result.currentWeight -= totalWeight;
+        return result.t;
+    }
+
+    /**
+     * 负载因子节点，保存了权重以及当前权重与负载因子的关系
+     * @param <T>
+     */
+    private static class Node<T> {
+        T t;
+        private int weight;
+        private int currentWeight;
+
+        public Node(T t, int weight) {
+            this.t = t;
+            this.weight = weight;
+            this.currentWeight = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        WeightedRoundRobinBalancer<String> blander = WeightedRoundRobinBalancer.instance();
+        blander.addInvoker("a", 4);
+        blander.addInvoker("b", 2);
+        blander.addInvoker("c", 1);
+        blander.addInvoker("d", 3);
+        for (int i = 0; i < 20; i++) {
+            System.out.println(blander.nextInvoker());
+        }
+    }
+}
---
+++ src/tree/TreeNode.java
@@ -0,0 +1,102 @@
+package tree;
+
+/**
+ * 二叉树节点Node
+ * 
+ * @author Lydck
+ *
+ * @param <T>
+ */
+public class TreeNode<T> {
+	/** 树节点存储的数据 */
+	T data;
+	/** parentNode */
+	TreeNode<T> pNode;
+	/** 左子节点（左子树） */
+	TreeNode<T> lNode;
+	/** 右子节点（右子树） */
+	TreeNode<T> rNode;
+	/** 初始化哈夫曼二叉树用到 */
+	boolean flag;
+	/** 哈夫曼树的叶子节点的权 */
+	int weight;
+
+	/**
+	 * 初始化哈夫曼树
+	 * 
+	 * @param weight
+	 *            权，正整数组成的数组
+	 * @return 哈夫曼树
+	 */
+	static <T> TreeNode<T> initHafmTree(int[] weight) {
+		int n = weight.length;
+		int maxWeight = 0;
+		// 初始化一个TreeNode列表,哈夫曼树的节点数为加权叶子节点数n的2n-1
+		@SuppressWarnings("unchecked")
+		TreeNode<T>[] tree = new TreeNode[2 * n - 1];
+		for (int i = 0; i < 2 * n - 1; i++) {
+			TreeNode<T> temp = new TreeNode<T>();
+			temp.flag = true;
+			if (i < n) {
+				maxWeight = weight[i] > maxWeight ? weight[i] : maxWeight;
+				temp.weight = weight[i];
+			} else {
+				// 非叶子节点的权为0
+				temp.weight = 0;
+			}
+			tree[i] = temp;
+		}
+		TreeNode<T> x1 = tree[0], x2 = tree[1];
+		// 构造哈弗曼树的n-1个非叶子节点
+		for (int i = 0; i < n - 1; i++) {
+			int m1 = maxWeight, m2 = maxWeight;
+			for (int j = 0; j < n + i; j++) {
+				if (tree[j].weight < m1 && tree[j].flag) {
+					x1 = tree[j];
+					m1 = x1.weight;
+				} else if (tree[j].weight < m2 && tree[j].flag) {
+					x2 = tree[j];
+					m2 = x2.weight;
+				}
+				
+			}
+			
+			x1.pNode = tree[n + i];
+			x2.pNode = tree[n + i];
+			x1.flag = false;
+			x2.flag = false;
+			tree[n + i].weight = x1.weight + x2.weight;
+			tree[n + i].lNode = x1;
+			tree[n + i].rNode = x2;
+		}
+		for(int i = 0; i < tree.length; i++) {
+			System.out.println(tree[i].weight);
+		}
+		return tree[n];
+	}
+
+	public static void main(String[] args) {
+		 TreeNode<Object> hafm = initHafmTree(new int[] {1, 2, 3, 4});
+		 System.out.println(hafm.weight);
+		/*int[] arr = new int[] { 0, 1, 2, 3, 4, 5 };
+		int temp = 0;
+		int a = 5, b = 5;
+		for (int j = 0; j < arr.length/2; j++) {
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] != -1 && arr[i] < a) {
+					a = arr[i];
+				} else if (arr[i] != -1 && arr[i] < b) {
+					b = arr[i];
+				}
+				if (a > b) {
+					temp = a;
+					a = b;
+					b = temp;
+				}
+			}
+			arr[a] = -1;
+			arr[b] = -1;
+			System.out.println(a + ", " + b);
+		}*/
+	}
+}
